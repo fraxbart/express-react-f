3ed7d77c53a049f4d39014858c93711c6161a634
fraxbart <119609549+fraxbart@users.noreply.github.com>
2023-05-28 16:46:03 +0200

    Update users.js

--- Back-End/models/users.js
+++ Back-End/models/users.js
@@ -1,41 +1,28 @@
 
-import mongoose from 'mongoose'
 import { Schema, model } from "mongoose"
-
-const UserSchema = new mongoose.Schema ({
-    firstname: {
-        type: String,
-required: true,
-max: 255
+const UserSchema = new Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
     },
-
-    lastname: {
-        type: String,
-        required: true
+    email:{
+        type:String,
+        required:true,
+        unique:true
     },
-
-
-    email: {
-        type: String,
-        required: true
+    password:{
+        type:String,
+        required:true
     },
-
-    password: {
-        type: String,
-        required: true
-    },
-
-    role: {
-        type: String,
-        required: false,
-        default: 0
-    },
-}, {
-    //opzione timestamps: quando utente salva i dati immessi si aggiorna le date di creazione ed aggiornamento ed opzione strict: accetta solo i dati che abbiamo richiesto
-    timestamps: true,
-    strict: true
+    posts:[{
+        type:Schema.Types.ObjectId,
+        ref: "Post"
+    }]
+},{
+    timestamps:true
 })
 
 const UserModel = model("User", UserSchema, "users");
 
-export default UserModel
+export default UserModel;
