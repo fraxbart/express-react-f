c71100936f0726ae747d3c1840a8cda92a50b69b
fraxbart <119609549+fraxbart@users.noreply.github.com>
2023-05-28 16:16:32 +0200

    Create SingleCard.js

---
+++ Front-End/src/Components/SingleCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+
+
+export const SingleCard = ({title, content, author, rate, img}) => {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={img} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>
+          {content.slice(0,40)}
+        </Card.Text>
+        <Card.Title>{author}</Card.Title>
+        <Card.Title>{rate}</Card.Title>
+      </Card.Body>
+    </Card>
+  )
+}
+
+
+export default SingleCard
