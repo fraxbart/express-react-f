652c48d39493987c02b71edca7776b29cd0fe4ea
fraxbart <119609549+fraxbart@users.noreply.github.com>
2023-06-09 21:46:17 +0200

    Add files via upload

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,23 +1,26 @@
-import Login from "./Pages/Login";
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import ProtectedRoutes from "./Middleware/ProtectedRoutes.js";
-import UsersList from "./Pages/UsersList";
+import './styles/App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import UserList from './pages/UserList.jsx'
+import ErrorPage from './pages/ErrorPage'
+import ProtectedRoutes from "./Middleware/ProtectedRoutes.js"
+
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route path="/users" element={<UsersList />} />
-        <Route element={<ProtectedRoutes />}>
-          <Route path="/home" element={<Home />} />
+        <Route exact path={'/'} element={<Login />} />
+        <Route path={'/users'} element={<UserList />} />
+        <Route element={<ProtectedRoutes />} >
+          <Route path={'/home'} element={<Home />} />
         </Route>
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -1,23 +1,25 @@
+import './styles/index.css'
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./Reducers/loginSlice";
 import postsReducer from "./Reducers/postsSlice";
+import addPostReducer from './Reducers/addNewPostSlice';
 
 const reducer = combineReducers({
   loginState: loginReducer,
   postsState: postsReducer,
-});
+  createPostState: addPostReducer
+})
 
 const store = configureStore({
-  reducer,
-});
+  reducer
+})
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -25,3 +27,5 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+
