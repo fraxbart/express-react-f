bb6aa4024f10cb346c2300c3763a8a39747f9255
fraxbart <119609549+fraxbart@users.noreply.github.com>
2023-06-11 22:31:39 +0200

    Add files via upload

--- Front-End/src/Components/forms/AddPostForm.js
+++ Front-End/src/Components/forms/AddPostForm.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import Button from 'react-bootstrap/Button'
+import { Button, Form } from 'react-bootstrap'
 import { getPosts } from '../../Reducers/postsSlice'
 import { addNewPost } from '../../Reducers/addNewPostSlice'
 import { Toast } from '../../utilities/notifications'
 import { Toaster } from "react-hot-toast"
+import useDecodedSession from '../../hooks/useDecodedSession'
 
 const AddPostForm = ({ close }) => {
     const successToast = new Toast('Post salvato correttamente')
@@ -14,6 +15,12 @@ const AddPostForm = ({ close }) => {
     const [formData, setFormData] = useState({})
 
     const dispatch = useDispatch()
+
+    const actualUser = useDecodedSession();
+    console.log(actualUser)
+
+    // const isAdmin = 'admin'
+    // const users = 'user'
 
     const onChangeHandleFile = (e) => {
         //sempre uguale per upload singolo
@@ -36,7 +43,6 @@ const AddPostForm = ({ close }) => {
             console.error('File upload error', error)
         }
     }
-    console.log(process.env.REACT_APP_CLOUD_SERVER_BASE_URL)
 
     const submitPost = async (e) => {
         e.preventDefault()
@@ -44,7 +50,6 @@ const AddPostForm = ({ close }) => {
         if (file) {
             try {
                 const uploadedFile = await upLoadFile(file)
-                console.log(upLoadFile)
                 const postsFormData = {
                     ...formData,
                     img: uploadedFile.img
@@ -84,17 +89,64 @@ const AddPostForm = ({ close }) => {
                             title: e.target.value
                         })}
                     />
-                    <input
-                        type="text"
-                        name="author"
-                        placeholder="Autore"
-                        className="text black rounded mb-2 p-2 border w-100"
-                        onChange={(e) => setFormData({
-                            ...formData,
-                            author: e.target.value
-                        })}
-                    />
+
+                    {/* SCELTA DELL'UTENTE */}
+                    <div>
+                        {actualUser && actualUser.role === 'admin' && <select
+                            name='author'
+                            placeholder='author'
+                            className="text-black rounded p-2 mb-2 border w-100"
+                            rows={8}
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    author: e.target.value
+                                })
+                            }
+                        >
+                            <option>Scegli autore</option>
+                            <option value={actualUser.id}>{actualUser.userName}</option>
+                        </select>}
+                    </div>
+
+                    <div>
+                        {actualUser && actualUser.role === 'user' && <select
+                            name='author'
+                            className="text-black rounded mb-2 p-2 border w-100"
+                            rows={8}
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    author: e.target.value
+                                })}
+                        >
+                            <option></option>
+                            <option value={actualUser.id}>{actualUser.userName}</option>
+                        </select>}
+                    </div>
+                    {/* FINE SCELTA UTENTE */}
+
+                    {/* {actualUser && isAdmin && (
+                        <select
+                            className="text-black rounded mb-2 p-2 border w-100"
+                            name='author'
+                            onChange={(e) => setFormData({
+                                ...formData,
+                                author: e.target.value,
+                            })
+                            }
+                        >
+                            <option>Seleziona l'uente</option>
+                            {users.users && users.users.map((item) => {
+                                return (
+                                    <option key={item._id} value={item._id}>{item.userName}</option>
+                                )
+                            })}
+                        </select>
+                    )} avrei preferito questa soluzione ma non funziona*/}
+
                     <select
+                        type="number"
                         name="rate"
                         placeholder="voto"
                         className="text-secondary form-select rounded mb-2 p-2 border w-100"
@@ -121,6 +173,7 @@ const AddPostForm = ({ close }) => {
                         content: e.target.value
                     })}
                 />
+
                 <div>
                     <input
                         name="img"
@@ -129,7 +182,7 @@ const AddPostForm = ({ close }) => {
                         onChange={onChangeHandleFile}
                     />
                 </div>
-                <div className="mt-2 d-flex justify-content-end">
+                <div className="d-grid gap-2">
                     <Button
                         type="submit"
                         variant="outline-warning"
